23bcabd854fdd02da91f88208b160991b42a540c
viejopinto <ja.pintoguerra@gmail.com>
2023-09-13 22:39:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Project Title
+
+Curso de Javascript
+Clase 1 - Conceptos introductorios
+
+## Authors
+
+- ASC/PUC KRUJOSKI, Saúl (Profe en Coderhouse)
+
+
+## Documentation
+
+El presente contiene algunas indicacioones sobre el contenido del proyecto usado como ejemplo.
+
+Como ser los siguientes:
+* Aprender la diferencia entre sitios y aplicaciones web.
+* Reconocer qué es un algoritmo y desarrollar capacidad resolutiva frente a un problema.
+* Comprender los fundamentos de Javascript.
+
+
+## Feedback
+
+Este proyecto es solamente de uso para ejemplo de clase, su estructura no puede ser o debiera ser tomada necesariamente como propuesta para un proyecto real.
+
+## Used By
+
+Este proyecto puede ser usado por los alumnos de la comisión 47060 o quién pueda aprovecharlo, para tener guías básicas de cómo abordar los conceptos que en este se intentan transmitir.
+
+Debiera ser acompañado junto al video de la clase en cuestión y el materíal teórico complementario.
+
---
+++ scripts/scripts.js
@@ -0,0 +1,32 @@
+
+// --Saludo al usuario
+
+alert("Bienvenido al buscador de descuentazos")
+alert("Presiona ok para continuar")
+
+// --Declaración de Variables
+let descuento = 0
+let totalPago = 0
+
+let respuestaUsuario = prompt("¿Quieres usar el buscador de descuentazos?"); // SI - NO
+while (respuestaUsuario.trim().toUpperCase() === "SI") {
+    let numId = parseInt(prompt("Ingresa tu número de identificación"))
+    let cat1 = prompt("Ingresa la categoría, solo puede ser Ropa, Videojuegos o Mascotas")
+    alert("Excelente, tenemos tu ID número " + numId + " registrado en la categoría " + cat1)
+    let cuantoGasta = parseInt(prompt("Cuánto es tu presupuesto para gastar"))
+    if (cuantoGasta < 15000) {
+        let montoExacto = parseFloat(prompt("Muy bien, ahora dinos cuánto exactamente vas a gastar, recuerda que tiene que ser menos de 15,000"))
+        descuento = montoExacto * 0.15
+        totalPago = montoExacto - descuento
+        alert ("El total a pagar es de " + totalPago)
+    }
+
+    else if (cuantoGasta >= 15000) {
+        let montoExacto = parseFloat(prompt("Muy bien, ahora dinos cuánto exactamente vas a gastar, recuerda que tiene que ser 15,000 o más"))
+        totalPago = montoExacto - (montoExacto * descuento)
+        alert ("El total a pagar es de " + totalPago)
+    }
+    respuestaUsuario = prompt("¿Quieres usar el buscador de descuentazos?"); // SI - NO
+}
+
+alert("Gracias")
